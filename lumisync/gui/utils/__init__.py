6f04b1598e9b24dedd45928b128b08efd7a6c1b2
mbsck <code@mbscheuck.com>
2026-03-01 22:06:44 +0100

    fix(OS): imports

--- lumisync/gui/utils/__init__.py
+++ lumisync/gui/utils/__init__.py
@@ -2,20 +2,24 @@
 Utility modules for the LumiSync GUI.
 """
 
-from .validators import IPAddressValidator, MACAddressValidator, PortValidator
-from .window_effects import (
-    apply_qt_translucent_background,
-    enable_windows_dwm_blur,
-    enable_windows_backdrop,
-    WindowsBackdropType,
-)
+from ...config.options import GENERAL
 
-__all__ = [
-    'IPAddressValidator',
-    'MACAddressValidator',
-    'PortValidator',
-    'apply_qt_translucent_background',
-    'enable_windows_dwm_blur',
-    'enable_windows_backdrop',
-    'WindowsBackdropType',
-]
+if GENERAL.platform == "Windows":
+
+    from .validators import IPAddressValidator, MACAddressValidator, PortValidator
+    from .window_effects import (
+        apply_qt_translucent_background,
+        enable_windows_dwm_blur,
+        enable_windows_backdrop,
+        WindowsBackdropType,
+    )
+
+    __all__ = [
+        "IPAddressValidator",
+        "MACAddressValidator",
+        "PortValidator",
+        "apply_qt_translucent_background",
+        "enable_windows_dwm_blur",
+        "enable_windows_backdrop",
+        "WindowsBackdropType",
+    ]
